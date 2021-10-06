aa3c00ec0d57dcf4f20a00aeb332a62418852376
ram.sinha <t-ramprasad.sinha@paypay-corp.co.jp>
2021-10-06 16:52:07 +0530

    api-view

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getroutes, name="routes"),
+    path('', views.getRoutes, name="routes"),
+    path('notes/',views.getNotes,name="notes"),
+    path('note/<str:pk>',views.getNote,name="note")
 ]
